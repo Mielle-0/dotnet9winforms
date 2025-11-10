bab5a7b36e559b27d5fa3d4bc8975d6f648a67f8
Mielle-0 <mckeljordan@gmail.com>
2025-11-10 13:09:13 +0800

    Initial Commit

---
+++ Data/DatabaseHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace it13Project.Data
+{
+    internal class DatabaseHelper
+    {
+
+        // Centralized connection string (you can move this to app.config for production)
+        private static readonly string connectionString =
+             @"Data Source=;Initial Catalog=;Integrated Security=True;Trust Server Certificate=True";
+            // @"Server=; Database=; User Id=; Password=; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True;";
+             
+        /// <summary>
+        /// Executes a SELECT query and returns a DataTable
+        /// </summary>
+        public static DataTable ExecuteQuery(string query, params SqlParameter[] parameters)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            using (var cmd = new SqlCommand(query, conn))
+            {
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+
+                using (var adapter = new SqlDataAdapter(cmd))
+                {
+                    var dt = new DataTable();
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Executes INSERT/UPDATE/DELETE and returns rows affected
+        /// </summary>
+        public static int ExecuteNonQuery(string query, params SqlParameter[] parameters)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            using (var cmd = new SqlCommand(query, conn))
+            {
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Executes a scalar query (returns a single value)
+        /// </summary>
+        public static object ExecuteScalar(string query, params SqlParameter[] parameters)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            using (var cmd = new SqlCommand(query, conn))
+            {
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+
+                conn.Open();
+                return cmd.ExecuteScalar();
+            }
+        }
+    }
+}
